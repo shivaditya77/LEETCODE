fadc9e14e64764711adac90f3a9b374856a9c9ae
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-03-29 03:28:41 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -10,18 +10,17 @@ public:
             int mid=start+(end-start)/2;
             if(target==nums[mid])
             {
-               leftmos=mid;
-               end=mid-1;
+                  leftmos=mid;
+                  end=mid-1;
             }
-            else if(nums[mid]>target)
-            {
-                end=mid-1;
-            }
-            else
+            else if(nums[mid]<target)
             {
                 start=mid+1;
             }
-
+            else
+            {
+                end=mid-1;
+            }
         }
         return leftmos;
     }
@@ -34,26 +33,30 @@ public:
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(target==nums[mid])
+            if(nums[mid]==target)
             {
-               rightmos=mid;
-               start=mid+1;
+                  rightmos=mid;
+                  start=mid+1;
             }
-            else if(nums[mid]>target)
-            {
-                end=mid-1;
-            }
-            else
+            else if(nums[mid]<target)
             {
                 start=mid+1;
             }
-
+            else
+            {
+                end=mid-1;
+            }
         }
         return rightmos;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left=leftmost(nums,target);
-        int right=rightmost(nums,target);
-        return {left,right};
+         int left=leftmost(nums,target);
+         int right=rightmost(nums,target);
+
+         vector<int>ans;
+         ans.push_back(left);
+         ans.push_back(right);
+
+         return ans;
     }
 };
