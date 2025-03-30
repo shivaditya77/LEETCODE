83c920534f3d7cd301cb4b0cfa31cae311a88e0c
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-03-31 01:39:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -10,32 +10,37 @@ public:
         {
             return 0;
         }
+
         if(nums[n-1]>nums[n-2])
         {
             return n-1;
         }
-        int low=1;
-        int high=n-2;
-        while(low<=high)
+
+        int start=1;
+        int end=n-2;
+
+        while(start<=end)
         {
-            int mid=low+(high-low)/2;
+            int mid=start+(end-start)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
             {
                 return mid;
             }
+            else if(nums[mid]<nums[mid+1])
+            {
+                start=mid+1;
+            }
             else if(nums[mid]>nums[mid-1])
             {
-                low=mid+1;
-            }
-            else if(nums[mid]>nums[mid+1])
-            {
-                high=mid-1;
+                end=mid-1;
             }
             else
             {
-                low=mid+1;
+                start=mid+1;
             }
         }
-        return -1;
+
+        return start;
+        
     }
 };
