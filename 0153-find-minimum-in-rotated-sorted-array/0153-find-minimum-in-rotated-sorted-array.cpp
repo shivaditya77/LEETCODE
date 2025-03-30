ec0ec7b8872d48d1f7cddc865483a1916a857a8e
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-03-30 16:27:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -12,13 +12,13 @@ public:
          while(start<end)
          {
             int mid=start+(end-start)/2;
-            if(nums[mid]>=nums[0])
+            if(nums[mid]>nums[0])
             {
                 start=mid+1;
             }
             else if(nums[mid]<nums[end])
             {
-                end=mid;
+                end=mid-1;
             }
          }
       return nums[start];
