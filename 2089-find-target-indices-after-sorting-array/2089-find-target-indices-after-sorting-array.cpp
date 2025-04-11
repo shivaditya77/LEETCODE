92a98af0a7e936c3b370f0e98674a2404f530f08
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-04-11 12:27:07 +0530

    Time: 0 ms (100%), Space: 15.3 MB (14.24%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,18 +1,105 @@
+// class Solution {
+// public:
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+//         sort(nums.begin(),nums.end());
+
+//         vector<int>ans;
+
+//         for(int i=0;i<nums.size();i++)
+//         {
+//            if(target==nums[i])
+//            {
+//             ans.push_back(i);
+//            }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-
-        vector<int>ans;
-
-        for(int i=0;i<nums.size();i++)
+    int leftmost(vector<int>& nums, int target)
+    {
+        int start=0;
+        int end=nums.size()-1;
+        int lefty;
+        while(start<=end)
         {
-           if(target==nums[i])
-           {
-            ans.push_back(i);
-           }
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                lefty=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
         }
 
-        return ans;
+        return lefty;
+    }
+
+    int rightmost(vector<int>& nums, int target)
+    {
+        int start=0;
+        int end=nums.size()-1;
+        int righty;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                righty=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return righty;
+         
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+
+          sort(nums.begin(),nums.end());
+          int flag=0;
+          for(int i=0;i<nums.size();i++)
+          {
+            if(nums[i]==target)
+            {
+                flag=1;
+            }
+          }
+          if(flag==0)
+          {
+            return {};
+          }
+
+           int left=leftmost(nums,target);
+           int right=rightmost(nums,target);
+
+           vector<int>ans;
+
+           while(left<=right)
+           {
+            ans.push_back(left);
+            left++;
+           }
+
+           return ans;
     }
 };
