81577debe7d293f2e5972aac4c296cc8edcc1a6b
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-05-24 10:41:36 -0400

    Time: 0 ms (100%), Space: 9.8 MB (22.36%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,19 +1,38 @@
+// class Solution {
+// public:
+//     int numIdenticalPairs(vector<int>& nums) {
+        
+//     //    brute force
+//     int count=0;
+//     for(int i=0;i<nums.size();i++)
+//     {
+//         for(int j=i+1;j<nums.size();j++)
+//         {
+//             if(nums[i]==nums[j])
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+// return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        
-    //    brute force
-    int count=0;
-    for(int i=0;i<nums.size();i++)
-    {
-        for(int j=i+1;j<nums.size();j++)
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==nums[j])
-            {
-                count++;
-            }
+            mp[nums[i]]++;
         }
-    }
-return count;
+         int total=0;
+        for(auto it:mp)
+        {
+          int save=it.second;
+          total=total+((save)*(save-1))/2;
+        }
+        return total;
     }
 };
