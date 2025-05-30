10c1a4432763391fb83c5ea8830086628b7a838d
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-05-30 00:56:55 -0400

    Time: 1 ms (33.04%), Space: 12.2 MB (72.42%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -10,8 +10,8 @@ public:
         int count=0;
         for(auto &it:mp)
         {
-            int nor=it.first+1;
-            int group=ceil(double(it.second)/nor);
+            int nor=it.first+1;//{1,1,2} 1->2  mtlb 1rabbit bol raha mere colour ka 1 rabbit hain toh khud nhi gin raha hain isliye total=it.first+1
+            int group=ceil(double(it.second)/nor);//kitne group mein rakhna hain rabbit ko usi ka formula hain suppose 2->1 1hi rabbit keh raha mere jaise 2 toh (1/3) 0 naa hojaye isliye ceil value le liye hain
             count+=(nor*group);
         }
         return count;
