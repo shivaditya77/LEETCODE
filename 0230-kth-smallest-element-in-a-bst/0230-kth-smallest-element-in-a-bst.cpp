b3f9f2ab36400068857a904c7b5636c0f91e0f7e
shivaditya77 <120047871+shivaditya77@users.noreply.github.com>
2025-11-26 20:59:32 +0530

    Time: 0 ms (100%), Space: 24.4 MB (66.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,21 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//      void iot(TreeNode* root,vector<int>&ans)
+//     {
+//         if(root==NULL)
+//         {
+//             return;
+//         }
+//         iot(root->left,ans);
+//         ans.push_back(root->val);
+//         iot(root->right,ans);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int>ans;
+//         iot(root,ans);
+//         return ans[k-1];
+//     }
+// };
+
+
 class Solution {
 public:
-     void iot(TreeNode* root,vector<int>&ans)
+     void iot(TreeNode* root,int &count,int &k,int &ans)
     {
         if(root==NULL)
         {
             return;
         }
-        iot(root->left,ans);
-        ans.push_back(root->val);
-        iot(root->right,ans);
+        iot(root->left,count,k,ans);
+        count++;
+        if(count==k)
+        {
+            ans=root->val;
+            return;
+        }
+        iot(root->right,count,k,ans);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>ans;
-        iot(root,ans);
-        return ans[k-1];
+        int ans=0;
+        int count=0;
+        iot(root,count,k,ans);
+        return ans;
     }
 };
